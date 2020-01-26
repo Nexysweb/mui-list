8526563e2f32d1a52d52825a442c0005854fc98c
Johan <johan@macair2020>
2020-01-26 12:08:23 +0100

    deleted component not used

--- src/lib/components/icon-button.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import IconButton from '@material-ui/core/IconButton';
-
-
-export default props => <IconButton className="icon-button" style={{padding: 3}} {...props} />;
--- src/lib/components/withDragDropContext.js
+++
@@ -1,4 +0,0 @@
-import {DragDropContext} from 'react-dnd-cjs';
-import HTML5Backend from 'react-dnd-html5-backend-cjs';
-
-export default DragDropContext(HTML5Backend);
