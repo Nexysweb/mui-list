1af832549c4927bbc78149f77b3b6884ec2a836a
Johan <johan@macair2020>
2020-01-26 12:15:32 +0100

    rename table folder

--- src/public.js
+++ src/public.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import { Code as CodeIcon } from '@material-ui/icons'
+
 const Default = () => (<div>
   <p>Select an option from the menu above</p>
 
-  <p><a href="https://github.com/Nexysweb/tabular-excel"><i className="fa fa-code"></i> Source</a>  available under MIT license.</p>
+  <p><a href="https://github.com/Nexysweb/mui-list"><CodeIcon/> Source</a>  available under MIT license.</p>
 </div>);
 
 export default Default;
