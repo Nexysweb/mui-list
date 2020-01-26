2d71d5a6361833592a4b6b0a9700b6393d9ad13f
Johan <johan@macair2020>
2020-01-26 12:08:31 +0100

    publish

--- docs/precache-manifest.92cacc602ec6319aaafcb57d9e61652f.js
+++ docs/precache-manifest.92cacc602ec6319aaafcb57d9e61652f.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ff5d7849a45f8ed118d107c9f843598e",
+    "revision": "232db4912acc008906e6392d5efe48f4",
     "url": "/mui-list/index.html"
   },
   {
-    "revision": "3d3f8e907c725f0401b8",
+    "revision": "3a1c10b75c9abcce65be",
     "url": "/mui-list/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "640d7b2b173cb45b4556",
-    "url": "/mui-list/static/js/2.9a5afc5a.chunk.js"
+    "revision": "82eb826da1c178493987",
+    "url": "/mui-list/static/js/2.c2032a6d.chunk.js"
   },
   {
-    "revision": "3d3f8e907c725f0401b8",
-    "url": "/mui-list/static/js/main.034569e4.chunk.js"
+    "revision": "3a1c10b75c9abcce65be",
+    "url": "/mui-list/static/js/main.5ad17eda.chunk.js"
   },
   {
     "revision": "fc4d0762b71e9f5a9f0b",
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mui-list/precache-manifest.58b365e7d3dc29e5cedf696d13b3609e.js"
+  "/mui-list/precache-manifest.92cacc602ec6319aaafcb57d9e61652f.js"
 );
 
 self.addEventListener('message', (event) => {
