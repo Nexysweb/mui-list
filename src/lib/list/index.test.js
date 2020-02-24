04cd52d1a0606f3941650780d8586a753c77ce24
John B <johan@nexys.ch>
2020-02-24 11:08:56 +0100

    Update index.test.js

--- src/lib/list/index.test.js
+++ src/lib/list/index.test.js
@@ -4,4 +4,5 @@ test('imports', () => {
   //expect(typeof Index.List).toEqual('function');
   expect(typeof Index.ListSuper).toEqual('function');
   expect(typeof Index.PaginationSuper).toEqual('function');
+  expect(typeof Index.OrderUtils).toEqual('object');
 })
